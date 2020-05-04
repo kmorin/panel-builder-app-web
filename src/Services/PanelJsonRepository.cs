7c484b6308ea9e587f61a2cea1129c154300c2e7
Kyle Morin <kyle.t.morin@gmail.com>
2020-05-04 15:43:47 -0700

    add db context

---
+++ src/Services/PanelJsonRepository.cs
@@ -0,0 +1,57 @@
+using System.Linq;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using panel_builder_app_web.Models;
+using System.Collections.Generic;
+
+namespace panel_builder_app_web.Services
+{
+    public class PanelJsonRepository : IPanelRepository
+    {
+        public List<Panel> Panels { get; private set; }
+
+        //Populate panels
+        public PanelJsonRepository()
+        {
+            var options = new JsonSerializerOptions{ PropertyNameCaseInsensitive = true};
+            string json = System.IO.File.ReadAllText("Api/panels.json");
+            try{ 
+              Panels = JsonSerializer.Deserialize<List<Panel>>(json, options);            
+            }
+            catch(Exception ex){ 
+              Console.WriteLine(ex);
+            }
+        }
+
+        public void Add<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            Panel panel = Panels.FirstOrDefault(x=>x.Id == id);
+            if (panel != null)
+            {
+                var didRemove = Panels.Remove(panel);
+                if (!didRemove) throw new Exception("Didn't delete");
+            }
+        }
+
+        public async Task<List<Panel>> GetAllPanelsAsync()
+        {
+            return Panels ?? null;
+        }
+
+        public async Task<Panel> GetPanelAsync(int id)
+        {
+            return Panels.FirstOrDefault(x=>x.Id == id);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/Services/PanelRepository.cs
+++ src/Services/PanelRepository.cs
@@ -1,57 +1,7 @@
-using System.Linq;
 using System;
-using System.Text.Json;
-using System.Threading.Tasks;
-using panel_builder_app_web.Models;
-using System.Collections.Generic;
-
-namespace panel_builder_app_web.Services
+namespace panel_builder_app_web
 {
-    public class PanelRepository : IPanelRepository
-    {
-        public List<Panel> Panels { get; private set; }
+    public class PanelRepository{
 
-        //Populate panels
-        public PanelRepository()
-        {
-            var options = new JsonSerializerOptions{ PropertyNameCaseInsensitive = true};
-            string json = System.IO.File.ReadAllText("Api/panels.json");
-            try{ 
-              Panels = JsonSerializer.Deserialize<List<Panel>>(json, options);            
-            }
-            catch(Exception ex){ 
-              Console.WriteLine(ex);
-            }
-        }
-
-        public void Add<T>(T entity) where T : class
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Delete(int id)
-        {
-            Panel panel = Panels.FirstOrDefault(x=>x.Id == id);
-            if (panel != null)
-            {
-                var didRemove = Panels.Remove(panel);
-                if (!didRemove) throw new Exception("Didn't delete");
-            }
-        }
-
-        public async Task<List<Panel>> GetAllPanelsAsync()
-        {
-            return Panels ?? null;
-        }
-
-        public async Task<Panel> GetPanelAsync(int id)
-        {
-            return Panels.FirstOrDefault(x=>x.Id == id);
-        }
-
-        public async Task<bool> SaveChangesAsync()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
